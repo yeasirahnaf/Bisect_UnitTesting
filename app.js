74a172a57450004f50e4dfe409a3c25b40bf2c56
yeasirahnaf <yeasir.ahnaf.1997@gmail.com>
2024-12-30 15:01:10 +0600

    Set up readline interface for user input

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Welcome to the calculator application!");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
